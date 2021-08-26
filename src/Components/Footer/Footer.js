f46374f5e8252f557c4e6200fab01d4052dc3a06
Hugo <blackfoxmmm@gmail.com>
2021-08-26 15:09:37 +0500

    finish

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import"./Footer.css";
+
+function Footer(){
+    return(
+        <>
+        <section className="section"> 
+          <h2 className="header2">Footer</h2>
+          <p className="p">lorem ipsum dolor nmadurlaa</p>
+        </section>
+       
+     </>
+    )
+}
+
+export default Footer;
